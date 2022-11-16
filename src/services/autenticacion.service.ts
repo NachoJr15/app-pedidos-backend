713f99c463d77eb54d6f6ad02ebefdad44a5bcac
hugo valencia jr <huvalpajr10@gmail.com>
2022-11-16 12:18:53 -0500

    Autenticaciones

---
+++ src/services/autenticacion.service.ts
@@ -0,0 +1,27 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import { repository } from '@loopback/repository';
+import { PersonaRepository } from '../repositories';
+const generador = require('password-generator');
+const cryptoJS = require('crypto-js');
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class AutenticacionService {
+  constructor(
+    @repository(PersonaRepository)
+    public personaRepository : PersonaRepository
+    ) {}
+
+  /*
+   * Add service methods here
+   */
+
+  GenerarClave(){
+    let clave = generador(8,false);
+    return clave;
+  }
+
+  CifrarClave(clave:string){
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+    return claveCifrada;
+  }
+}
